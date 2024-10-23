f5c4b6bfa6377aaced4c44956a16b8d89a9ebb07
Ifeoluwa <os.ifeoluwa@gmail.com>
2024-10-23 05:30:57 +0100

    MENTRUAL CYCLE USE CASE, MENSTRUAL CYCLE CLASSDIAGRAM AND PROTOTYPE (YET TO BE COMPLETED)

---
+++ MenstrualCycleApp/MenstrualCycleApplication.java
@@ -0,0 +1,38 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class MenstrualCycleApplication {
+
+	public static void main(String[] args) {
+		Scanner cycle = new Scanner(System.in);
+		DateTimeFormatter dateConverter = DateTimeFormatter.ofPattern("day/month/year");
+
+		try {
+		System.out.print("Enter the start date of the last cycle (Your submission should be in Day/Month/Year format): ");
+		String lastCycleDate = cycle.nextLine();
+		LocalDate startDate = LocalDate.parse(lastCycleDate, dateConverter);
+
+
+
+
+
+
+
+
+
+
+
+		} catch (DateTimeParseException err) {
+			System.out.println("Invalid date format: " + err.getMessage());
+
+		
+		}
+
+
+
+	}
+
+
+}
