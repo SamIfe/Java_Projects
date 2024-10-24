c0dad41bd2997fe1830712e5a7bf1d2ff9aa0cfa
Ifeoluwa <os.ifeoluwa@gmail.com>
2024-10-24 10:45:24 +0100

    Updated score grade(Yet to be completed)

--- GradeScore/StudentGrade.java
+++ GradeScore/StudentGrade.java
@@ -4,28 +4,39 @@ import java.util.Scanner;
 public class StudentGrade {
 	public static void main (String...arg) {
 		Scanner grade = new Scanner (System.in);
-		System.out.println ("How many students do you have?");
+		System.out.print ("\n How many students do you have? : ");
 		int numberOfStudents = grade.nextInt();
 
-		System.out.println ("How many subject do they offer? ");
+		System.out.print ("How many subject do they offer? : ");
 		int numberOfSubjects = grade.nextInt();
-		System.out.println ("Saving >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-		System.out.println ("Saved successfully");
+		System.out.println ("\nSaving >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		System.out.println ("Saved successfully \n\n");
 
 
-
-
+		double scores = 0;
+		double average = 0;
 		double [][] collectScores = new double [numberOfStudents] [numberOfSubjects];
 		
 		for (int row = 0; row < numberOfStudents; row++) {
 			System.out.println ("Entering score for student " + (row + 1));
 
 		for (int column = 0; column < numberOfSubjects; column++) {
-			System.out.println ("Enter score for subject + (column + 1)");
+
+			do {
+				System.out.print ("Enter score for subject " + (column + 1) + ": ");
+				scores = grade.nextDouble();
+				if (scores <= 0 || scores >= 100) {
+					System.out.println("Invalid input; please enter number of scores between 0 and 100 ");
+			
+				}
+
+			} while (scores <= 0 || scores >= 100);
+			collectScores [row][column] = scores;
 
 		}
-			System.out.println ("Saving >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-			System.out.println ("Saved successfully");
+		
+			System.out.println ("\n Saving >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+			System.out.println ("Saved successfully \n");
 
 		}
 
