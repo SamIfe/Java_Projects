dd6e111cc9a307f4b8ce91a3bd5a809751dd6d5b
Ifeoluwa <os.ifeoluwa@gmail.com>
2024-10-26 17:34:48 +0100

    Updated task

--- GateOne/NewSnacks/PhoneBookApp.java
+++ GateOne/NewSnacks/PhoneBookApp.java
@@ -1,12 +1,25 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.ArrayList;
 public class PhoneBookApp {
 	public static void main (String...args) {
 
+
+
+System.out.print("""
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+AFEEZ'S PHONEBOOK APPLICATIONS FOR EFFICIENT CONTACT MANAGEMENT.
+
+
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+""");
+
 		Scanner sc = new Scanner (System.in);
-		System.out.print ("How many contact do you want to save? ");
-		int contact = sc.nextInt();
-		String [] contactInfo = new String [contact];
+		//System.out.print ("How many contact do you want to save? ");
+		//int contact = sc.nextInt();
+		ArrayList<String> contactInfo = new ArrayList<>();
 
 		System.out.println("""
 		Main Menu
@@ -26,18 +39,30 @@ public class PhoneBookApp {
 			case 1:
 				System.out.println ("Enter first name: ");
 				String firstName = sc.nextLine();
-				sc.nextLine();
 
 				System.out.println ("Enter last name: ");
-				String lastname = sc.nextLine();
+				String lastName = sc.nextLine();
 				
 				
 				System.out.print ("Enter phone number: ");
 				String phoneNumber = sc.nextLine();
 
-				for (int check = 0; check < contactInfo.length(); check++) {
+				contactInfo.add(firstName + " " + lastName + " - " + phoneNumber);
+
+
+				System.out.println("====================================================");
+
+				
+
+				for(String element : contactInfo) {
+					System.out.println(element);
+
 
 				}
+				System.out.println("The contact has been added successfully");
+
+				System.out.println("====================================================");
+
 
 				
 
@@ -50,3 +75,4 @@ public class PhoneBookApp {
 
 
 }
+
