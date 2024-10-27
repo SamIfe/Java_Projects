25673c3dc70d395475a122d84bcb254d6826b3de
Ifeoluwa <os.ifeoluwa@gmail.com>
2024-10-27 14:01:02 +0100

    Progress update

--- GateOne/OldSnacks/CheckOutApplication/CheckOutApplication.java
+++ GateOne/OldSnacks/CheckOutApplication/CheckOutApplication.java
@@ -2,42 +2,50 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class CheckOutApplication {
 	public static void main(String [] args) {
-	ArrayList<String> goods =  new ArrayList<>();
-	ArrayList<Integer> quantity = new ArrayList<>();
-	ArrayList<Integer> price = new ArrayList<>();
+		ArrayList<String> itemsPurchased =  new ArrayList<>();
+		ArrayList<Integer> quantityPurchased = new ArrayList<>();
+		ArrayList<Integer> priceForEach = new ArrayList<>();
 
-	Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 	
-	System.out.println("What is the customer's Name :");
-	String name = input.nextLine();
+		System.out.println("Enter customer's name :");
+		String name = input.nextLine();
 	
-	int flag = 1;
-	while(flag == 1) {
+		int control = 1;
+		while(control == 1) {
 	
-	System.out.println("What did the user buy ? ");
-	goods.add (input.next());
+			System.out.println("What items did the user purchase? ");
+			itemsPurchased.add (input.next());
 	
-	System.out.println("How many pieces ?" );
-	int pieces = input.nextInt();
-	while(pieces < 1) {
+			System.out.println("How many pieces ?" );
+			int pieces = input.nextInt();
+			while(pieces < 1) {
 
-	System.out.println("How many pieces ?" );
-	pieces = input.nextInt();
+				System.out.println("How many pieces ?" );
+				pieces = input.nextInt();
+			}
+			quantityPurchased.add (pieces);
+	
+				System.out.println("How much per unit ?");
+				int unit = input.nextInt();
+			while(unit < 1) {
+				System.out.println("How much per unit ?");
+				unit = input.nextInt();
+
+			}
+
+			priceForEach.add(unit);
+	
+		System.out.println("Enter 1 to add more items or any value to stop: ");
+		control = input.nextInt();
+
+
+
+
 		}
-	quantity.add (pieces);
-	
-	System.out.println("How much per unit ?");
-	int unit = input.nextInt();
-	while(unit < 1) {
-		System.out.println("How much per unit ?");
-		unit = input.nextInt();
-
-		}
-
-	
 
 
 
-}
+
 	}
 }	
